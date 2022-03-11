f58857b216f764f37e637838087bbba88a6cef92
Keven Arroyo <dake.3601@gmail.com>
2022-03-11 15:20:37 -0600

    Minor Fix

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-  app.delete('/api/persons/:id', (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
